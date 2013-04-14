ed651a840e378ef0da823bc2ef23fb46626d0921
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2013-04-13 22:51:20 -0400

    Commiting to master, hope this works

---
+++ Project1/js/json.js
@@ -0,0 +1,162 @@
+var json=
+{
+"contact1":
+      {
+      "lastName": 			["Last Name:", "Downey"],
+      "phoneNumber":		["Phone Number:", "803-412-7815"],
+      "restaurant":			["Restaurant", "Charlotte`s Web"],
+      "numberOfPeople":     ["Number Of People", "two"]
+      },
+
+"contact2":
+      {
+      "lastName": 			["Last Name:", "Stark"],
+      "phoneNumber":		["Phone Number:", "844-668-8263"],
+      "restaurant":			["Restaurant", "Hornets Nest"],
+      "numberOfPeople":     ["Number Of People", "six"]
+      },
+
+"contact3":
+      {
+      "lastName": 			["Last Name:", "Hooks"],
+      "phoneNumber":		["Phone Number:", "811-062-5865"],
+      "restaurant":			["Restaurant", "Mint Hill"],
+      "numberOfPeople":     ["Number Of People", "three"]
+      },
+
+"contact4":
+      {
+      "lastName": 			["Last Name:", "Early"],
+      "phoneNumber":		["Phone Number:", "855-440-8804"],
+      "restaurant":			["Restaurant", "Mint Hill"],
+      "numberOfPeople":     ["Number Of People", "two"]
+      },
+
+"contact5":
+      {
+      "lastName": 			["Last Name:", "Ratcliff"],
+      "phoneNumber":		["Phone Number:", "822-829-1924"],
+      "restaurant":			["Restaurant", "Veterans Park"],
+      "numberOfPeople":     ["Number Of People", "four"]
+      },
+
+"contact6":
+      {
+      "lastName": 			["Last Name:", "Seals"],
+      "phoneNumber":		["Phone Number:", "803-705-5475"],
+      "restaurant":			["Restaurant", "Sugaw Creek"],
+      "numberOfPeople":     ["Number Of People", "six"]
+      },
+
+"contact7":
+      {
+      "lastName": 			["Last Name:", "Ingram"],
+      "phoneNumber":		["Phone Number:", "803-414-0327"],
+      "restaurant":			["Restaurant", "Hornets Nest"],
+      "numberOfPeople":     ["Number Of People", "five"]
+      },
+
+"contact8":
+      {
+      "lastName": 			["Last Name:", "Woodard"],
+      "phoneNumber":		["Phone Number:", "803-279-3420"],
+      "restaurant":			["Restaurant", "Charlotte`s Web"],
+      "numberOfPeople":     ["Number Of People", "six"]
+      },
+
+"contact9":
+      {
+      "lastName": 			["Last Name:", "Matthew"],
+      "phoneNumber":		["Phone Number:", "803-533-8731"],
+      "restaurant":			["Restaurant", "Winthrop University"],
+      "numberOfPeople":     ["Number Of People", "two"]
+      },
+
+"contact10":
+      {
+      "lastName": 			["Last Name:", "Castillo"],
+      "phoneNumber":		["Phone Number:", "803-647-8212"],
+      "restaurant":			["Restaurant", "Sugaw Creek"],
+      "numberOfPeople":     ["Number Of People", "six"]
+      },
+
+"contact11":
+      {
+      "lastName": 			["Last Name:", "Daugherty"],
+      "phoneNumber":		["Phone Number:", "803-301-9480"],
+      "restaurant":			["Restaurant", "Mint Hill"],
+      "numberOfPeople":     ["Number Of People", "six"]
+      },
+
+"contact12":
+      {
+      "lastName": 			["Last Name:", "Mora"],
+      "phoneNumber":		["Phone Number:", "803-524-8684"],
+      "restaurant":			["Restaurant", "Veterans Park"],
+      "numberOfPeople":     ["Number Of People", "two"]
+      },
+
+"contact13":
+      {
+      "lastName": 			["Last Name:", "Wilder"],
+      "phoneNumber":		["Phone Number:", "803-999-4146"],
+      "restaurant":			["Restaurant", "Mint Hill"],
+      "numberOfPeople":     ["Number Of People", "five"]
+      },
+
+"contact14":
+      {
+      "lastName": 			["Last Name:", "Pruett"],
+      "phoneNumber":		["Phone Number:", "803-226-8660"],
+      "restaurant":			["Restaurant", "Sugaw Creek"],
+      "numberOfPeople":     ["Number Of People", "five"]
+      },
+
+"contact15":
+      {
+      "lastName": 			["Last Name:", "Cook"],
+      "phoneNumber":		["Phone Number:", "803-961-9532"],
+      "restaurant":			["Restaurant", "Winthrop University"],
+      "numberOfPeople":     ["Number Of People", "three"]
+      },
+
+"contact16":
+      {
+      "lastName": 			["Last Name:", "Churchill"],
+      "phoneNumber":		["Phone Number:", "803-647-8212"],
+      "restaurant":			["Restaurant", "Hornets Nest"],
+      "numberOfPeople":     ["Number Of People", "four"]
+      },
+
+"contact17":
+      {
+      "lastName": 			["Last Name:", "Norris"],
+      "phoneNumber":		["Phone Number:", "855-691-8585"],
+      "restaurant":			["Restaurant", "Veterans Park"],
+      "numberOfPeople":     ["Number Of People", "four"]
+      },
+
+"contact18":
+      {
+      "lastName": 			["Last Name:", "Ramos"],
+      "phoneNumber":		["Phone Number:", "899-053-9012"],
+      "restaurant":			["Restaurant", "Charlotte`s Web"],
+      "numberOfPeople":     ["Number Of People", "three"]
+      },
+
+"contact19":
+      {
+      "lastName": 			["Last Name:", "Douglass"],
+      "phoneNumber":		["Phone Number:", "822-232-0018"],
+      "restaurant":			["Restaurant", "Winthrop University"],
+      "numberOfPeople":     ["Number Of People", "two"]
+      },
+      
+"contact20":
+      {
+      "lastName": 			["Last Name:", "Tucker"],
+      "phoneNumber":		["Phone Number:", "803-412-7815"],
+      "restaurant":			["Restaurant", "Veterans Park"],
+      "numberOfPeople":     ["Number Of People", "six"]
+      }
+}
---
+++ Project1/js/main.js
@@ -0,0 +1,136 @@
+//Brenna Pavlinchak
+//CRUD Project
+//ASD 1304
+ 
+
+        $('#home').on('pageinit', function()
+        {
+
+                        
+        });
+        
+        $('#addItem').on('pageinit', function()
+        {
+        
+              $('#saveData').on('click', function()
+              {
+                  var key;
+                  var id;
+                  
+                    if(!key)
+                    {
+                       id = Math.floor(Math.random()*1000001);    
+                    }
+                    
+                    else
+                    {
+                        id = key;
+                    }
+                    
+                    var item = JSON.stringify(
+                    {
+                        lastName        : $("#lastName").val(),
+                        phoneNumber     : $("#phoneNumber").val(),
+                        numberOfPeople  : $("#numberOfPeople").val(),
+                        park            : $("#park").val()
+                    });
+                    
+                    localStorage.setItem(id, JSON.stringify(item));
+                    alert("The game was saved.");
+              });
+                       
+
+                        
+        });
+        
+        $('#dataPage').on('pageinit', function()
+        {
+            var foo = 0;
+            if(foo == 0)
+            {
+            $("#gameList").empty();
+        
+            $.ajax({    
+                       url: 'xhr/data.json', //What i am getting
+                       type: 'GET', //We are getting not posting  
+                       dataType: 'json', //Getting JSON data, located in data.json   
+                       success: function(data)
+                       {  
+                              for(var i=0, j=data.json.length; i<j; i++)//for loop to read the whole json
+                              {
+                                    var game = data.json[i];
+                                    
+                                    $(''+
+                                    '<li>'+ game.lastName +'</li>'+
+                                    '<li>'+ game.phoneNumber +'</li>'+
+                                    '<li>'+ game.numberOfPeople +'</li>'+
+                                    '<li>'+ game.park +'</li>'
+                                    ).appendTo("#gameList").listview;
+                              }
+                              $('#gameList').listview('refresh');
+                        } 
+                });
+                
+                 } 
+            
+            else
+            {
+	            alert("im in the else"); 
+	            
+	            localStorage.getItem(id, JSON.stringify(item));
+	        
+		        for(var i=0, j=data.json.length; i<j; i++)//for loop to read the whole json
+	            {
+	                 var game = data.json[i];
+	                                    
+	                 $(''+
+	                 '<li>'+ game.lastName +'</li>'+
+	                 '<li>'+ game.phoneNumber +'</li>'+
+	                 '<li>'+ game.numberOfPeople +'</li>'+
+	                 '<li>'+ game.park +'</li>'
+	                 ).appendTo("#gameList").listview;
+	            }
+	            $('#gameList').listview('refresh');
+	          
+            }
+            
+         });
+         
+         
+       
+       
+        $('#ajaxPage').on('pageinit', function()
+        {
+          
+        
+          $("#ajaxList").empty();
+          
+          $.ajax({
+                    url: 'xhr/data.xml',
+                    type: 'GET',
+                    dataType: 'xml',
+                    success: function(xml)
+                    {
+                           console.log(xml);
+                            
+                            $(xml).find('ajax').each(function()
+                            {
+                              var xml = {};
+                              xml.lastName       = $(this).find('lastName').text();
+                              xml.phoneNumber    = $(this).find('phoneNumber').text();
+                              xml.numberOfPeople = $(this).find('numberOfPeople').text();
+                              xml.park           = $(this).find('park').text();
+                              
+                              
+                              $(''+
+                                '<li>'+ xml.lastName +'</li>'+
+                                '<li>'+ xml.phoneNumber +'</li>'+
+                                '<li>'+ xml.numberOfPeople +'</li>'+
+                                '<li>'+ xml.park +'</li>'
+                                ).appendTo('#ajaxList');
+                            });
+                            $('#ajaxList').listview('refresh');
+                    }   
+                });  
+           
+        }); 
