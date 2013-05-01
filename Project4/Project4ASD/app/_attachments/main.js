ff85a5526b44a82863b200e49b3b8b2083e99a23
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2013-05-01 10:59:35 -0400

    Chaged the places of a few things lets see if it works

--- Project4/Project4ASD/app/_attachments/main.js
+++ Project4/Project4ASD/app/_attachments/main.js
@@ -71,26 +71,28 @@
             });
            
             
-            var urlVars = function()
-            { 
-                var urlData = $($.mobile.activePage).data("url");
-                console.log(urlData)
-                var urlParts = urlData.split('?');
-                var urlPairs = urlParts[1].split('&');
-                var urlValues = {};
-                
-                    for(var pair in urlPairs)
-                    {
-                        var keyValue = urlPairs[pair].split('=');
-                        var key = decodeURIComponent(keyValue[0]);
-                        var value = decodeURIComponent(keyValue[1]);
-                        urlValues[key] = value;
-                    }
-                    return urlValues;
+           
             };
 			
             $(document).on('pageinit', '#lastName', function()
             {
+            	 var urlVars = function()
+                 { 
+                     var urlData = $($.mobile.activePage).data("url");
+                     console.log(urlData)
+                     var urlParts = urlData.split('?');
+                     var urlPairs = urlParts[1].split('&');
+                     var urlValues = {};
+                     
+                         for(var pair in urlPairs)
+                         {
+                             var keyValue = urlPairs[pair].split('=');
+                             var key = decodeURIComponent(keyValue[0]);
+                             var value = decodeURIComponent(keyValue[1]);
+                             urlValues[key] = value;
+                         }
+                         return urlValues;
+            	
                  var lastName = urlVars()["item.lastName"];
                  
                         
